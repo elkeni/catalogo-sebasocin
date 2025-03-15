002c2b449cc9d4a1c71a454b18f9555712a17d5f
Stefano <sroselg051@gmail.com>
2025-03-15 13:40:33 -0500

    Fix rutas en Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Asegura que las rutas funcionen correctamente
+  base: '/',
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    historyApiFallback: true
+  }
 });
